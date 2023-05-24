32e682ac8cf77b64f6a5c5b38457998f91f27932
김주희 <woolfie@naver.com>
2023-05-24 22:12:11 +0900

    .

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,10 @@ public class SecurityConfig {
 
         http
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/mypage").hasRole("USER")
+                .requestMatchers("/messages").hasRole("MANAGER")
+                .requestMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
             );
 
@@ -23,5 +32,32 @@ public class SecurityConfig {
             .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        String password = passwordEncoder().encode("1111");
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password(password)
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    // public void configure(AuthenticationManagerBuilder auth) throws Exception{
+    //
+    //     String password = passwordEncoder().encode("1111");
+    //
+    //     auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+    //     auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+    //     auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    // }
+
+    //평문인 패스워드를 암호화한다.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
