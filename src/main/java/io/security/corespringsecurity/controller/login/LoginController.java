fdd5c0ec15ba00207b20aba4d976b90f11dacbb2
김주희 <woolfie@naver.com>
2023-05-30 23:07:42 +0900

    실전프로젝트 - 인증 프로세스 Ajax 인증 구현 - csrf설정

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,54 +1,55 @@
 package io.security.corespringsecurity.controller.login;
 
+import java.security.Principal;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.security.token.AjaxAuthenticationToken;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
+    @RequestMapping(value={"/login", "/api/login"})
     public String login(@RequestParam(value = "error", required = false) String error,
-                        @RequestParam(value = "exception", required = false) String exception, Model model){
-
-        model.addAttribute("error", error);
-        model.addAttribute("exception", exception);
-
-        return "user/login/login";
+        @RequestParam(value = "exception", required = false) String exception, Model model){
+        model.addAttribute("error",error);
+        model.addAttribute("exception",exception);
+        return "login";
     }
 
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        if(authentication != null){
-            //logout 처리
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null){
             new SecurityContextLogoutHandler().logout(request, response, authentication);
         }
 
         return "redirect:/login";
     }
 
-    // @GetMapping(value="/denied")
-    // public String accessDenied() throws Exception {
-    //
-    //     return "user/login/denied";
-    // }
+    @GetMapping(value={"/denied","/api/denied"})
+    public String accessDenied(@RequestParam(value = "exception", required = false) String exception, Principal principal, Model model) throws Exception {
 
-    @GetMapping("/denied")
-    public String accessDenied(@RequestParam(value = "exception", required = false) String exception, Model model){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Account account = (Account)authentication.getPrincipal();
+        Account account = null;
+
+        if (principal instanceof UsernamePasswordAuthenticationToken) {
+            account = (Account) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
+
+        }else if(principal instanceof AjaxAuthenticationToken){
+            account = (Account) ((AjaxAuthenticationToken) principal).getPrincipal();
+        }
         model.addAttribute("username", account.getUsername());
         model.addAttribute("exception", exception);
 
